d680f69f0b89e692c2ee491941daa62f38d9d755
Katye Russell <katye.russell@gmail.com>
2015-02-09 19:46:03 -0600

    Added app file for later.  Created basic form structure in index.html and started JavaScript for form submission.

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,39 @@
 function hello() {
   return 'world';
 }
+
+
+
+
+// Above is some space for tests.  Down here is the actual JS code...
+//
+//Make the form appear on 'add new contact' click
+
+var $form = $('form');
+
+$form.submit(function(evt) {
+  evt.preventDefault();
+
+  var $firstName = $('input[name="firstName"]');
+  $firstName.val('');
+
+
+});
+
+
+
+// POST form data to firebase
+//
+// Add data to contact list in firebase
+//
+// Make the form disappear
+//
+// getJSON call for the information
+//
+// Create divs for each contact
+//
+// Append contact data to the new divs
+//
+// Remove contact button goes with each div
+//
+// Contact info gets removed from both page and database on click
